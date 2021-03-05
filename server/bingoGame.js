8c4ec23e8fc50bb09bf6595cacd3d4e241d35bc3
DeBray Carpenter <debraycarpenter@DeBrays-MacBook-Pro.local>
2021-03-05 11:04:40 -0800

    updated bingoGame.js file

--- server/bingoGame.js
+++ server/bingoGame.js
@@ -8,6 +8,16 @@ class BingoGame {
     this.calledNumbers = ['FREE'];
     this.cards = [];
     this.totalCards = this.cards.length;
+    this.numbers = [
+      1,2,3,4,5,6,7,8,9,10,
+      11,12,13,14,15,16,17,18,19,20,
+      21,22,23,24,25,26,27,28,29,30,
+      31,32,33,34,35,36,37,38,39,40,
+      41,42,43,44,45,46,47,48,49,50,
+      51,52,53,54,55,56,57,58,59,60,
+      61,62,63,64,65,66,67,68,69,70,
+      71,72,73,74,75
+    ];
   }
   spawnCards(player){
     if (player.numCards == 1) {
@@ -22,8 +32,24 @@ class BingoGame {
       }
     }
   }
+  callNumber(){
+    let { numbers, calledNumbers } = this;
+    let temp = getRandomObj(numbers);
+    let { num } = temp;
+    if (!calledNumbers.includes(num)) {
+      calledNumbers.push(num);
+      return temp;
+    } else {
+      while(calledNumbers.includes(temp.number)){
+        temp  = getRandomObj(numbers);
+      }
+      calledNumbers.push(num);
+      return temp;
+    }
+  }
   checkForWinners(){
     if (this.type == 'blackout') {
+      // Blackout B.I.N.G.O. game
       let numMatches = 0;
       this.cards.map(card => {
         let {B, I, N, G, O} = card;
@@ -45,6 +71,7 @@ class BingoGame {
         // O
       })
     } else {
+      // Regular B.I.N.G.O. game
 
     }
   }
